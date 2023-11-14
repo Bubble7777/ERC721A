6c0c056e8e88e7b77a63b41b49943b67417a7e96
Bubble7777 <sdmmds93@gmail.com>
2023-11-15 02:43:04 +0600

    add comments for functions

--- contracts/BubbleToken721A.sol
+++ contracts/BubbleToken721A.sol
@@ -25,6 +25,7 @@ contract BubbleToken is ERC721A, Ownable {
         root = _root;
     }
 
+    // функция для минта за 0,01
     function safeMint(address to,uint256 quantity) external payable pausable {
         uint256 totalPrice = quantity * price;
         if(msg.value < totalPrice ){
@@ -33,6 +34,7 @@ contract BubbleToken is ERC721A, Ownable {
         _safeMint(to, quantity);
     } 
     
+    // функция для минта кто в белом списке работает с помощью дерева меркл
      function safeMintWhiteList(address to,uint256 quantity, bytes32[] memory proof) external pausable {
         bytes32 _leaf = keccak256(abi.encodePacked(_msgSender()));
         if(isValid(proof, _leaf)){
@@ -40,26 +42,31 @@ contract BubbleToken is ERC721A, Ownable {
         }
     }
 
+    // функция для минта владельца контракта
     function safeMintOwner(address to,uint256 quantity) external pausable onlyOwner{
             _safeMint(to, quantity);
         } 
 
+    // функция для проверки есть ли в корневом root
     function isValid(bytes32[] memory  proof,bytes32 leaf) public view returns(bool){
         return MerkleProof.verify(proof,root,leaf);
     }
 
+    //функция для изменения цены
     function setPrice(uint256 _PriceInWEI) external onlyOwner {
         price = _PriceInWEI;
     }
 
+    //функция для влючение или отключение паузы
     function setPause() external onlyOwner {
         pause = !pause;
     }
 
+    //функция для изменения root 
     function setRoot(bytes32 _root) external onlyOwner {
         root = _root;
     }
-
+    // функция для вывода средств из контракта 
     function withdraw() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
